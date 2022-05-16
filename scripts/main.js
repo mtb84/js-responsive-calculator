be7c9201f47fa6c09bd61237a1d11c5450a511b9
MATTHEW TYLER BARTON <mtb@MATTHEWs-MacBook-Pro.local>
2022-05-16 01:28:46 -0400

    works but is buggy

--- scripts/main.js
+++ scripts/main.js
@@ -8,45 +8,101 @@ const percent = document.querySelector(`.percent`);
 const clear = document.querySelector(`.clear`);
 const calculatorKeys = document.querySelectorAll(`calculator-keys`);
 const calculatorScreen = document.querySelector(`.calculator-screen`)
+const plusMinus = document.querySelector(`.plus-minus`);
 
-const calculation = [];
-const calculate = function (event) {
-    return alert(event.target.value);
-}
-const pushNumber = function (event) {
-    return alert(event.target.value);
-}
+equalSign.addEventListener('click', pushEquals);
+percent.addEventListener('click', pushPercent);
+clear.addEventListener('click', pushClear);
 
-// math function 
+let calculation = [];
+let operator;
+let shownNumber = 0;
+let result;
 
-const operators = {
-    "+": function (num1, num2) {
-           return num1 + num2;
-         },
-    "-": function (num1, num2) {
-           return num1 - num2;
-         },
-    "*": function (num1, num2) {
-           return num1 * num2;
-         },
-    "/": function (num1, num2) {
-           return num1 / num2;
-         },
-    "%": function (num1, num2) {
-           return num1 % num2;
-         },
+function calcNumber() {
+    if (calculation.length === 3) {
+      calculate();
+    } else if (calculation.length > 3) {
+     }
+  };
+
+  function pushNumber(num) {
+    if (calculatorScreen.value === '0') {
+        calculatorScreen.value = num.target.value;
+    } else {
+        calculatorScreen.value = calculatorScreen.value + num.target.value;
+    };
+  };
+
+  function pushNonNumber(operator) {
+    shownNumber = Number(calculatorScreen.value);
+    if (result !== shownNumber && shownNumber !== 0) {
+      calculation.push(shownNumber)
+      calcNumber()
+    };
+    calculation.push(operator)
+    calculatorScreen.value = '';
+    calcNumber();
+  }
+
+  function pushOperator(op) {
+	const operator = op.target.value;
+  pushNonNumber(operator);
+  }
+
+  function pushPercent(perc) {
+    const operator = perc.target.value;
+    pushNonNumber(operator);
+  }
+
+  function pushEquals() {
+    if (calculation.length === 2) {
+        shownNumber = Number(calculatorScreen.value);
+      calculation.push(shownNumber);
+      calcNumber();
+    };
+  }
+
+  function pushClear() {
+    calculation = [];
+    result = 0;
+    calculatorScreen.value = '0';
+  }  
+
+  function calculate() {
+    let num1 = Number(calculation[0]);
+    const operatorEntered = calculation[1];
+    let num2 = Number(calculation[2]);
+    result = operators[operatorEntered](num1, num2);
+    calculatorScreen.value = result;
+    calculation = [result];
   };
 
 
+  for (let i = 0; i < numberButton.length; i++) {
+    numberButton[i].addEventListener('click', pushNumber);
+  };
+  
+  for (let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener('click', pushOperator);
+  };
 
-numberButton.forEach;
-    function button() {
-    addEventListener(`click`, pushNumber)
-};
-button();
 
-operatorButtons.forEach;
-    function button() {
-    addEventListener(`click`, pushNumber)
-};
-button();
+const operators = {
+    '+': function (num1, num2) {
+           return num1 + num2;
+         },
+    '-': function (num1, num2) {
+           return num1 - num2;
+         },
+    '*': function (num1, num2) {
+           return num1 * num2;
+         },
+    '/': function (num1, num2) {
+           return num1 / num2;
+         },
+    '%': function (num1, num2) {
+           return num1 % num2;
+         },
+
+  };
